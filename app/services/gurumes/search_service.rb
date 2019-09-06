c05d35ab5fdabb1d71986bdc4a913d06c19f1e43
Kohei Shinsawa <kshinsawa@netprotections.co.jp>
2019-09-06 19:51:24 +0900

    use credentials.yml.enc

--- app/services/gurumes/search_service.rb
+++ app/services/gurumes/search_service.rb
@@ -7,8 +7,7 @@ module Gurumes
     end
 
     def run
-      api_id = "22bdbb2bc86a783507f6acff85f6606c"
-      uri = URI.parse URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{api_id}&address=#{@area}&freeword=#{@food}&&hit_per_page=#{@hit}")
+      uri = URI.parse URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{Rails.application.credentials.gnavi[:access_key_id]}&address=#{@area}&freeword=#{@food}&&hit_per_page=#{@hit}")
       response = JSON.load(Net::HTTP.get(uri))
       response["rest"]
     end
